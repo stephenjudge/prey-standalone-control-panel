a6175973a8b33564cf9e62e38f4a25fb1c8e3ed4
Tomás Pollak <tomaspollak@gmail.com>
2011-10-27 22:49:26 -0300

    Return an XML response. This lets us trigger modules and also set the delay.

--- models.rb
+++ models.rb
@@ -4,7 +4,9 @@ MongoMapper.database = ENV['mongo_db'] || 'prey_standalone_control_panel'
 
 class Device
 	include MongoMapper::Document
-	
+
 	key :name, String, :required => true
 	key :missing, Boolean, :default => false
+	key :delay, Integer, :default => 20
+	key :module_list, String, :default => 'geo network session webcam'
 end
--- routes.rb
+++ routes.rb
@@ -1,15 +1,45 @@
 require 'sinatra'
+require 'builder'
 
 module Prey
+
+	REPORT_MODULES = %w(geo network session webcam)
 
 	class Standalone < Sinatra::Base
 
 		get '/:id' do
 			if device = Device.find(params[:id])
+				content_type 'text/xml'
 				status device.missing? ? 404 : 200
+				generate_xml(device)
 			else
 				status 400 # bad request
 			end
+		end
+
+		def generate_xml(device)
+			builder do |xml|
+				xml.instruct!
+				xml.device do
+					xml.status do
+						xml.missing device.missing?
+					end
+					xml.configuration do
+						xml.delay device.delay
+					end
+					xml.modules do
+						device.module_list.split.each do |mod|
+							xml.module :active => true, :name => mod, :type => module_type(mod) do
+								xml.enabled true
+							end
+						end
+					end
+				end
+			end
+		end
+
+		def module_type(mod)
+			REPORT_MODULES.include?(mod) ? 'report' : 'action'
 		end
 
 	end
